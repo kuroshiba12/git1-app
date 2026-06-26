d1091162a6d53a3752cb85fd0206ec8416cdd18b
kuroshiba12 <k023c0019@g.neec.ac.jp>
2026-06-26 10:23:16 +0900

    NeonDB接続追加

--- app/api/hello/route.ts
+++ app/api/hello/route.ts
@@ -1,6 +1,12 @@
+import { neon } from '@neondatabase/serverless';
+
 export async function GET() {
+  const sql = neon(`${process.env.DATABASE_URL}`);
+  const rows = await sql`SELECT message, created_at FROM test_table LIMIT 1`;
+  const row = rows[0];
+
   return Response.json({
-    message: "Hello from server!",
-    createdAt: new Date().toISOString(),
+    message: row.message,
+    createdAt: row.created_at,
   });
 }
